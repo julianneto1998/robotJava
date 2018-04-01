5f4748452b7dd6c52d781e623153b0391111c4ef
Jeremy Tsang <jeremy@jeremytsang.com>
2018-04-01 01:14:41 -0700

    Add comments for test case

--- src/Driver.java
+++ src/Driver.java
@@ -6,9 +6,9 @@ public class Driver {
 
 	DCMotor dcmotor = new DCMotor(2, 3);
 
-	dcmotor.setDirection(1, 1);
+	dcmotor.setDirection(1, 1);  // run the motor forward for 1 second
 
-	dcmotor.setDirection(-1, 3);
+	dcmotor.setDirection(-1, 3);  // run the motor backwards for 3 seconds
 
     }
 }
